2980eccf4cf743cb4e0823d111214574e228d785
Artem Kurianov <artemkuryanov@gmail.com>
2021-02-05 09:29:14 +0000

    Add project files.

---
+++ Interfaces/IHitBtcClient.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HitBtc.Net.Interfaces
+{
+    public interface IHitBtcClient
+    {
+        //TODO: decribe actions according to docs
+    }
+}
---
+++ Interfaces/IHitBtcSocketClient.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HitBtc.Net.Interfaces
+{
+    interface IHitBtcSocketClient
+    {
+    }
+}
