4d3025df3ff6e90553a9345eca0b37c8faa7e39f
Artem Kurianov <artemkuryanov@gmail.com>
2021-02-05 09:50:54 +0000

    market data init. currencies

--- Interfaces/IHitBtcClient.cs
+++ Interfaces/IHitBtcClient.cs
@@ -1,11 +1,29 @@
-﻿using System;
+﻿using CryptoExchange.Net.Objects;
+using HitBtc.Net.Objects.MarketData;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace HitBtc.Net.Interfaces
 {
     public interface IHitBtcClient
     {
         //TODO: decribe actions according to docs
+        #region Market data
+        /// <summary>
+        /// Get a list of all currencies or specified currencies (GET /api/2/public/currency)
+        /// </summary>
+        /// <param name="currencies">Comma-separated list of currency codes. Optional parameter</param>
+        /// <returns></returns>
+        Task<WebCallResult<HitBtcCurrency>> GetCurrenciesAsync(params string[] currencies);
+        /// <summary>
+        /// Get a list of all currencies or specified currencies (GET /api/2/public/currency)
+        /// </summary>
+        /// <param name="currencies">Comma-separated list of currency codes. Optional parameter</param>
+        /// <returns></returns>
+        WebCallResult<HitBtcCurrency> GetCurrencies(params string[] currencies);
+
+        #endregion
     }
 }
---
+++ Objects/MarketData/HitBtcCurrency.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HitBtc.Net.Objects.MarketData
+{
+    public class HitBtcCurrency
+    {
+        /// <summary>
+        /// Currency identifier (code), for example, ''BTC''    Note: description will simply use currency in the future.
+        /// </summary>
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        /// <summary>
+        /// Currency full name, for example, ''Bitcoin''
+        /// </summary>
+        [JsonProperty("fullName")]
+        public string FullName { get; set; }
+
+        [JsonProperty("crypto")]
+        public bool Crypto { get; set; }
+
+        [JsonProperty("payinEnabled")]
+        public bool PayinEnabled { get; set; }
+
+        [JsonProperty("payinPaymentId")]
+        public bool PayinPaymentId { get; set; }
+
+        [JsonProperty("payinConfirmations")]
+        public long PayinConfirmations { get; set; }
+
+        [JsonProperty("payoutEnabled")]
+        public bool PayoutEnabled { get; set; }
+
+        [JsonProperty("payoutIsPaymentId")]
+        public bool PayoutIsPaymentId { get; set; }
+
+        [JsonProperty("transferEnabled")]
+        public bool TransferEnabled { get; set; }
+
+        [JsonProperty("delisted")]
+        public bool Delisted { get; set; }
+
+        [JsonProperty("payoutFee")]
+        public string PayoutFee { get; set; }
+
+        [JsonProperty("payoutMinimalAmount")]
+        public string PayoutMinimalAmount { get; set; }
+
+        [JsonProperty("precisionPayout")]
+        public long PrecisionPayout { get; set; }
+
+        [JsonProperty("precisionTransfer")]
+        public long PrecisionTransfer { get; set; }
+    }
+}
